2952f1cc7f35c231ef8cf7c7c60f2920a78ff9ea
chauhaonguyen2012 <chauhaonguyen2012@gmail.com>
2025-10-11 15:00:35 +0700

    done

---
+++ final.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6 import uic 
+import sys
+
+class Login(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("home worlk 77\untitled.ui", self)
+        
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Login()
+    window.show()
+    sys.exit(app.exec())
+    sys
+
