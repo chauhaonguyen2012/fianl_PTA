1fc4cbf471107094fb800ce85bd7685ebc145c8d
chauhaonguyen2012 <chauhaonguyen2012@gmail.com>
2025-11-01 15:50:41 +0700

    done

--- final.py
+++ final.py
@@ -1,16 +1,47 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt6 import uic 
 import sys
+import os
 
 class Login(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("login.ui", self)
-        
+        self.btnLogin.clicked.connect(self.check_login)
+        self.new_account.clicked.connect(self.show_register)
+        self.msg_box = QMessageBox()
+
+    def check_login(self):
+        email = self.Email.text()
+        password = self.Password.text()
+        if email == "owner" and password == "owner":
+            main.show()
+            self.close()
+
+    def show_register(self):
+        register.show()
+        self.close()
+
+class Register(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("creat.ui", self)
+        self.creat.clicked.connect(self.back_to_login)
+        self.msg_box = QMessageBox()
+
+    def back_to_login(self):
+        login.show()
+        self.close()
+    
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("main.ui", self)
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Login()
-    window.show()
-    sys.exit(app.exec())
-    sys
-
+    login = Login()
+    register = Register()
+    main = Main()
+    login.show()
+    app.exec()
